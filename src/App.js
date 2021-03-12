880b213c3fd53cad6d0d2a910ed679da8499f694
marco corapi <marco.corapi@outlook.it>
2021-03-12 18:35:36 +0100

    uuid def

--- src/App.js
+++ src/App.js
@@ -1,3 +1,21 @@
+//####################################
+// UUID
+//####################################
+
+/*
+  UUID is literally a package you can add in React to generate you a random
+  ID. In fact, in the previous notes we were deleting a tweet referencing only to the message 
+  itself. The problem was that we were relaying on the message to delete the tweet.
+  What if we had 4 equal messages. We delete all of them then. Because we delete things
+  based on the text and not some sort of ID. In fact, UUID, is the solution.
+
+  So we want to tell React what exactly we are deleting, example whether it is a tweet or
+  youtube video. And we need something concrete there. Not a title or description but something
+  concrete there. We need a "Universally Unique Identifiers". Usually APIs, have ID data
+  integrated. But when you do not use APIs for instance, you gonna need them.
+
+  */
+
 import './App.css';
 import TweetList from './components/TweetList.js'
 import CreateTweet from './components/CreateTweet.js'
